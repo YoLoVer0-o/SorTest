329736c56b3ae8cd7cfa78921b1500d75be4ddae
ufazaa55 <ufazaa55@gmail.com>
2023-08-25 10:46:12 +0700

    test

--- 3DJsExp/src/pages/Dashboard.jsx
+++ 3DJsExp/src/pages/Dashboard.jsx
@@ -1,11 +1,16 @@
 import Pie from '../components/Pie'
 import Bar from '../components/Bar'
+import bankData from '../assets/MOCK_DATA.jsx'
 function Dashboard() {
 
     return (
         <div>
             <div>
-                <Pie  />
+                <Pie data={bankData}
+                    width={200}
+                    height={200}
+                    innerRadius={60}
+                    outerRadius={100} />
             </div>
             <div>
                 <Bar />
