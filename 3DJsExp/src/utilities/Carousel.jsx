d55332fb104352d5641e93cb78a1eb7ea1a24455
ภูริพัฒน์ กมลเนตร์ <phooriphat.k@rtarf.mi.th>
2023-09-28 10:56:08 +0700

    allPic

--- 3DJsExp/src/utilities/Carousel.jsx
+++ 3DJsExp/src/utilities/Carousel.jsx
@@ -11,35 +11,43 @@ import poster from "../assets/poster.jpg";
 //   textAlign: "center",
 //   background: "#364d79",
 // };
-const CarouselReport = () => (
-
-  <Carousel autoplay className="tw-flex tw-w-[30rem] tw-justify-self-center tw-content-center  tw-justify-center  tw-bg-red-900 ">
-    <div className="tw-flex tw-justify-center tw-object-center  tw-max-w-80 tw-max-h-80	">
-      <img
-        className="tw-flex tw-justify-center tw-w-[30rem] tw-h-[20rem] tw-max-w-[inherit] object-contain tw-max-h-[inherit]" 
-        src={carouselPic}
-      />
-    </div>
-    <div className=" tw-flex tw-justify-center tw-object-center  tw-max-w-80 tw-max-h-80">
-      <img  src={poster}
+const CarouselReport = (props) => {
+  const showAll = props.onClick
+  return (
+    <div className=" tw-relative tw-flex tw-flex-col">
+      <button onClick={showAll} className="tw-absolute tw-mt-1 tw-mr-1 tw-justify-center tw-items-center tw-flex tw-self-end tw-bg-gray-100/80 tw-rounded-md tw-h-8 tw-text-lg tw-z-10">รูปภาพทั้งหมด</button>
+      <Carousel
+        autoplay
+        className="tw-flex  tw-w-[30rem] tw-justify-self-center tw-content-center tw-justify-center  tw-bg-red-900 tw-z-0"
+      >
+        <div className="tw-flex tw-justify-center tw-object-center  tw-max-w-80 tw-max-h-80 	">
+          <img
             className="tw-flex tw-justify-center tw-w-[30rem] tw-h-[20rem] tw-max-w-[inherit] object-contain tw-max-h-[inherit]"
-           
-           />
+            src={carouselPic}
+          />
+        </div>
+        <div className=" tw-flex tw-justify-center tw-object-center  tw-max-w-80 tw-max-h-80">
+          <img
+            src={poster}
+            className="tw-flex tw-justify-center tw-w-[30rem] tw-h-[20rem] tw-max-w-[inherit] object-contain tw-max-h-[inherit]"
+          />
+        </div>
+        {/* <div className="tw-flex tw-justify-center tw-w-[20rem] tw-h-[20rem]">
+    <img 
+  //   style={contentStyle} 
+    src={carouselPic} 
+    className="tw-max-w-[inherit] tw-max-h-[inherit]"
+    />
+  </div>
+  <div className="tw-flex tw-justify-center tw-w-[20rem] tw-h-[20rem]">
+    <img 
+  //   style={contentStyle} 
+    src={carouselPic}
+    className="tw-max-w-[inherit] tw-max-h-[inherit]"
+    />
+  </div> */}
+      </Carousel>
     </div>
-    {/* <div className="tw-flex tw-justify-center tw-w-[20rem] tw-h-[20rem]">
-      <img 
-    //   style={contentStyle} 
-      src={carouselPic} 
-      className="tw-max-w-[inherit] tw-max-h-[inherit]"
-      />
-    </div>
-    <div className="tw-flex tw-justify-center tw-w-[20rem] tw-h-[20rem]">
-      <img 
-    //   style={contentStyle} 
-      src={carouselPic}
-      className="tw-max-w-[inherit] tw-max-h-[inherit]"
-      />
-    </div> */}
-  </Carousel>
-);
+  );
+};
 export default CarouselReport;
---
+++ 3DJsExp/src/utilities/PicModal.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Modal } from "antd";
+import carouselPic from "../assets/carouselPic.jpg";
+import poster from "../assets/poster.jpg";
+
+const PicModal = (props) => {
+  const modalToggle = props.modalToggle;
+  const handleCancel = props.handleCancel;
+  //   const modalData = props.modalData;
+
+  const [isModalOpen, setIsModalOpen] = useState(modalToggle);
+  //   const [data, setData] = useState(modalData);
+
+  //   useEffect(() => {
+  //     //console.log("data selected");
+  //     setData(modalData);
+  //   }, [modalData]);
+
+  useEffect(() => {
+    setIsModalOpen(modalToggle);
+  }, [modalToggle]);
+
+  return (
+    <>
+      <Modal
+        className=" tw-w-max tw-h-max tw-max-h-fit tw-max-w-fit tw-bg-slate-100/0  tw-z-0"
+        title="ภาพทั้งหมด"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <div className=" tw-flex  tw-flex-row tw-z-10 tw-w-max tw-h-max tw-gap-6 tw-opacity-100">
+            <img className=" tw-rounded-md tw-w-96 tw-h-72 tw-z-10 tw-opacity-100 tw-drop-shadow-[7px_10px_5px_4px_rgba(0,0,0,0.75)]	" src={carouselPic}/>
+            <img className=" tw-rounded-md  tw-w-96 tw-h-72 tw-z-10 tw-opacity-100 tw-drop-shadow-lg	" src={poster}/>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+PicModal.propTypes = {
+  modalToggle: PropTypes.bool.isRequired,
+  handleCancel: PropTypes.func.isRequired,
+};
+
+export default PicModal;
