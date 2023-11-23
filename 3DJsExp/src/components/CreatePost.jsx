4d8c98963fd1d18502d3b73e63ca72249c7b24ae
ภูริพัฒน์ กมลเนตร์ <phooriphat.k@rtarf.mi.th>
2023-11-23 16:53:43 +0700

    responsive fixing

--- 3DJsExp/src/components/CreatePost.jsx
+++ 3DJsExp/src/components/CreatePost.jsx
@@ -25,7 +25,7 @@ const CreatePost = () => {
   }
 
   return (
-    <div className="tw-w-screen tw-h-full tw-p-4">
+    <div className="tw-w-screen tw-h-full tw-p-4 tw-overflow-y-auto">
       <div className="tw-flex tw-justify-center ">Createpost</div>
       <SelectPlatform onPlatformSelect={handlePlatformSelect} />
       <hr className="tw-h-px tw-my-8 tw-bg-gray-200 tw-border-0 dark:tw-bg-gray-700"></hr>
