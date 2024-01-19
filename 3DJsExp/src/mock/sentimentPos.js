eeffb15de81c3c36fc7f8d0e44ee31d2b99c6acc
ufazaa55 <ufazaa55@gmail.com>
2024-01-19 09:36:23 +0700

    fix mock

--- 3DJsExp/src/mock/sentimentPos.js
+++ 3DJsExp/src/mock/sentimentPos.js
@@ -1,102 +1,572 @@
 const sentimentPos = [
   {
-    name: "Shoes",
+    name: "พูดถูก",
+    value: 30,
+    commentType: "positive",
+    Comment: [
+      {
+        id: 1,
+        timestamp: "2022/09/04",
+        userName: "A",
+        message: "ศาล และ สถาบัน จะอยู่อย่างไร",
+      },
+      {
+        id: 2,
+        timestamp: "2022/09/04",
+        userName: "B",
+        message: "เปิดเมนท์อื่นด้วยเจียม",
+      },
+      {
+        id: 3,
+        timestamp: "2022/09/04",
+        userName: "C",
+        message:
+          "บังคับให้คนรัก ทั้งๆที่ตัวมันและครอบครัว กอบโกยทุกอย่าง ไม่เคยเปนแบบอย่างที่ดีให้กับแผ่นดิน ไม่นานพวกมันก็ล่มสลายไปเอง",
+      },
+      {
+        id: 4,
+        timestamp: "2022/09/04",
+        userName: "D",
+        message: "จารเปิดคอมเม้นแล้วหรอ",
+      },
+      {
+        id: 5,
+        timestamp: "2022/09/04",
+        userName: "E",
+        message: "เสียของที่ไม่เคยมีได้ด้วยเหรอ",
+      },
+      {
+        id: 6,
+        timestamp: "2022/09/04",
+        userName: "F",
+        message: "หายใจบางแล้วเหรอเจียม",
+      },
+      {
+        id: 7,
+        timestamp: "2022/09/04",
+        userName: "G",
+        message: "คงอัดอั้นนะ ที่ สศจ ไม่เปิดเม้นให้ด่า",
+      },
+      {
+        id: 8,
+        timestamp: "2022/09/04",
+        userName: "H",
+        message:
+          "คนที่จ้องจะโจมตีเจียมจริงๆแบบหวังผลอ่ะ คือ io เพื่อไทย จะบอกให้",
+      },
+    ],
+  },
+  {
+    name: "สู้ๆ",
+    value: 25,
+    commentType: "positive",
+    Comment: [
+      {
+        id: 1,
+        timestamp: "2022/09/04",
+        userName: "A",
+        message: "ศาล และ สถาบัน จะอยู่อย่างไร",
+      },
+      {
+        id: 2,
+        timestamp: "2022/09/04",
+        userName: "B",
+        message: "เปิดเมนท์อื่นด้วยเจียม",
+      },
+      {
+        id: 3,
+        timestamp: "2022/09/04",
+        userName: "C",
+        message:
+          "บังคับให้คนรัก ทั้งๆที่ตัวมันและครอบครัว กอบโกยทุกอย่าง ไม่เคยเปนแบบอย่างที่ดีให้กับแผ่นดิน ไม่นานพวกมันก็ล่มสลายไปเอง",
+      },
+      {
+        id: 4,
+        timestamp: "2022/09/04",
+        userName: "D",
+        message: "จารเปิดคอมเม้นแล้วหรอ",
+      },
+      {
+        id: 5,
+        timestamp: "2022/09/04",
+        userName: "E",
+        message: "เสียของที่ไม่เคยมีได้ด้วยเหรอ",
+      },
+      {
+        id: 6,
+        timestamp: "2022/09/04",
+        userName: "F",
+        message: "หายใจบางแล้วเหรอเจียม",
+      },
+      {
+        id: 7,
+        timestamp: "2022/09/04",
+        userName: "G",
+        message: "คงอัดอั้นนะ ที่ สศจ ไม่เปิดเม้นให้ด่า",
+      },
+      {
+        id: 8,
+        timestamp: "2022/09/04",
+        userName: "H",
+        message:
+          "คนที่จ้องจะโจมตีเจียมจริงๆแบบหวังผลอ่ะ คือ io เพื่อไทย จะบอกให้",
+      },
+    ],
+  },
+  {
+    name: "เปิดเม้น",
+    value: 23,
+    commentType: "positive",
+    Comment: [
+      {
+        id: 1,
+        timestamp: "2022/09/04",
+        userName: "A",
+        message: "ศาล และ สถาบัน จะอยู่อย่างไร",
+      },
+      {
+        id: 2,
+        timestamp: "2022/09/04",
+        userName: "B",
+        message: "เปิดเมนท์อื่นด้วยเจียม",
+      },
+      {
+        id: 3,
+        timestamp: "2022/09/04",
+        userName: "C",
+        message:
+          "บังคับให้คนรัก ทั้งๆที่ตัวมันและครอบครัว กอบโกยทุกอย่าง ไม่เคยเปนแบบอย่างที่ดีให้กับแผ่นดิน ไม่นานพวกมันก็ล่มสลายไปเอง",
+      },
+      {
+        id: 4,
+        timestamp: "2022/09/04",
+        userName: "D",
+        message: "จารเปิดคอมเม้นแล้วหรอ",
+      },
+      {
+        id: 5,
+        timestamp: "2022/09/04",
+        userName: "E",
+        message: "เสียของที่ไม่เคยมีได้ด้วยเหรอ",
+      },
+      {
+        id: 6,
+        timestamp: "2022/09/04",
+        userName: "F",
+        message: "หายใจบางแล้วเหรอเจียม",
+      },
+      {
+        id: 7,
+        timestamp: "2022/09/04",
+        userName: "G",
+        message: "คงอัดอั้นนะ ที่ สศจ ไม่เปิดเม้นให้ด่า",
+      },
+      {
+        id: 8,
+        timestamp: "2022/09/04",
+        userName: "H",
+        message:
+          "คนที่จ้องจะโจมตีเจียมจริงๆแบบหวังผลอ่ะ คือ io เพื่อไทย จะบอกให้",
+      },
+    ],
+  },
+  {
+    name: "เห็นด้วย",
+    value: 22,
+    commentType: "positive",
+    Comment: [
+      {
+        id: 1,
+        timestamp: "2022/09/04",
+        userName: "A",
+        message: "ศาล และ สถาบัน จะอยู่อย่างไร",
+      },
+      {
+        id: 2,
+        timestamp: "2022/09/04",
+        userName: "B",
+        message: "เปิดเมนท์อื่นด้วยเจียม",
+      },
+      {
+        id: 3,
+        timestamp: "2022/09/04",
+        userName: "C",
+        message:
+          "บังคับให้คนรัก ทั้งๆที่ตัวมันและครอบครัว กอบโกยทุกอย่าง ไม่เคยเปนแบบอย่างที่ดีให้กับแผ่นดิน ไม่นานพวกมันก็ล่มสลายไปเอง",
+      },
+      {
+        id: 4,
+        timestamp: "2022/09/04",
+        userName: "D",
+        message: "จารเปิดคอมเม้นแล้วหรอ",
+      },
+      {
+        id: 5,
+        timestamp: "2022/09/04",
+        userName: "E",
+        message: "เสียของที่ไม่เคยมีได้ด้วยเหรอ",
+      },
+      {
+        id: 6,
+        timestamp: "2022/09/04",
+        userName: "F",
+        message: "หายใจบางแล้วเหรอเจียม",
+      },
+      {
+        id: 7,
+        timestamp: "2022/09/04",
+        userName: "G",
+        message: "คงอัดอั้นนะ ที่ สศจ ไม่เปิดเม้นให้ด่า",
+      },
+      {
+        id: 8,
+        timestamp: "2022/09/04",
+        userName: "H",
+        message:
+          "คนที่จ้องจะโจมตีเจียมจริงๆแบบหวังผลอ่ะ คือ io เพื่อไทย จะบอกให้",
+      },
+    ],
+  },
+  {
+    name: "สศจ",
+    value: 18,
+    commentType: "positive",
+    Comment: [
+      {
+        id: 1,
+        timestamp: "2022/09/04",
+        userName: "A",
+        message: "ศาล และ สถาบัน จะอยู่อย่างไร",
+      },
+      {
+        id: 2,
+        timestamp: "2022/09/04",
+        userName: "B",
+        message: "เปิดเมนท์อื่นด้วยเจียม",
+      },
+      {
+        id: 3,
+        timestamp: "2022/09/04",
+        userName: "C",
+        message:
+          "บังคับให้คนรัก ทั้งๆที่ตัวมันและครอบครัว กอบโกยทุกอย่าง ไม่เคยเปนแบบอย่างที่ดีให้กับแผ่นดิน ไม่นานพวกมันก็ล่มสลายไปเอง",
+      },
+      {
+        id: 4,
+        timestamp: "2022/09/04",
+        userName: "D",
+        message: "จารเปิดคอมเม้นแล้วหรอ",
+      },
+      {
+        id: 5,
+        timestamp: "2022/09/04",
+        userName: "E",
+        message: "เสียของที่ไม่เคยมีได้ด้วยเหรอ",
+      },
+      {
+        id: 6,
+        timestamp: "2022/09/04",
+        userName: "F",
+        message: "หายใจบางแล้วเหรอเจียม",
+      },
+      {
+        id: 7,
+        timestamp: "2022/09/04",
+        userName: "G",
+        message: "คงอัดอั้นนะ ที่ สศจ ไม่เปิดเม้นให้ด่า",
+      },
+      {
+        id: 8,
+        timestamp: "2022/09/04",
+        userName: "H",
+        message:
+          "คนที่จ้องจะโจมตีเจียมจริงๆแบบหวังผลอ่ะ คือ io เพื่อไทย จะบอกให้",
+      },
+    ],
+  },
+  {
+    name: "ครอบครัว",
+    value: 15,
+    commentType: "positive",
+    Comment: [
+      {
+        id: 1,
+        timestamp: "2022/09/04",
+        userName: "A",
+        message: "ศาล และ สถาบัน จะอยู่อย่างไร",
+      },
+      {
+        id: 2,
+        timestamp: "2022/09/04",
+        userName: "B",
+        message: "เปิดเมนท์อื่นด้วยเจียม",
+      },
+      {
+        id: 3,
+        timestamp: "2022/09/04",
+        userName: "C",
+        message:
+          "บังคับให้คนรัก ทั้งๆที่ตัวมันและครอบครัว กอบโกยทุกอย่าง ไม่เคยเปนแบบอย่างที่ดีให้กับแผ่นดิน ไม่นานพวกมันก็ล่มสลายไปเอง",
+      },
+      {
+        id: 4,
+        timestamp: "2022/09/04",
+        userName: "D",
+        message: "จารเปิดคอมเม้นแล้วหรอ",
+      },
+      {
+        id: 5,
+        timestamp: "2022/09/04",
+        userName: "E",
+        message: "เสียของที่ไม่เคยมีได้ด้วยเหรอ",
+      },
+      {
+        id: 6,
+        timestamp: "2022/09/04",
+        userName: "F",
+        message: "หายใจบางแล้วเหรอเจียม",
+      },
+      {
+        id: 7,
+        timestamp: "2022/09/04",
+        userName: "G",
+        message: "คงอัดอั้นนะ ที่ สศจ ไม่เปิดเม้นให้ด่า",
+      },
+      {
+        id: 8,
+        timestamp: "2022/09/04",
+        userName: "H",
+        message:
+          "คนที่จ้องจะโจมตีเจียมจริงๆแบบหวังผลอ่ะ คือ io เพื่อไทย จะบอกให้",
+      },
+    ],
+  },
+  {
+    name: "หวังผล",
+    value: 15,
+    commentType: "positive",
+    Comment: [
+      {
+        id: 1,
+        timestamp: "2022/09/04",
+        userName: "A",
+        message: "ศาล และ สถาบัน จะอยู่อย่างไร",
+      },
+      {
+        id: 2,
+        timestamp: "2022/09/04",
+        userName: "B",
+        message: "เปิดเมนท์อื่นด้วยเจียม",
+      },
+      {
+        id: 3,
+        timestamp: "2022/09/04",
+        userName: "C",
+        message:
+          "บังคับให้คนรัก ทั้งๆที่ตัวมันและครอบครัว กอบโกยทุกอย่าง ไม่เคยเปนแบบอย่างที่ดีให้กับแผ่นดิน ไม่นานพวกมันก็ล่มสลายไปเอง",
+      },
+      {
+        id: 4,
+        timestamp: "2022/09/04",
+        userName: "D",
+        message: "จารเปิดคอมเม้นแล้วหรอ",
+      },
+      {
+        id: 5,
+        timestamp: "2022/09/04",
+        userName: "E",
+        message: "เสียของที่ไม่เคยมีได้ด้วยเหรอ",
+      },
+      {
+        id: 6,
+        timestamp: "2022/09/04",
+        userName: "F",
+        message: "หายใจบางแล้วเหรอเจียม",
+      },
+      {
+        id: 7,
+        timestamp: "2022/09/04",
+        userName: "G",
+        message: "คงอัดอั้นนะ ที่ สศจ ไม่เปิดเม้นให้ด่า",
+      },
+      {
+        id: 8,
+        timestamp: "2022/09/04",
+        userName: "H",
+        message:
+          "คนที่จ้องจะโจมตีเจียมจริงๆแบบหวังผลอ่ะ คือ io เพื่อไทย จะบอกให้",
+      },
+    ],
+  },
+  {
+    name: "แผ่นดิน",
+    value: 14,
+    commentType: "positive",
+    Comment: [
+      {
+        id: 1,
+        timestamp: "2022/09/04",
+        userName: "A",
+        message: "ศาล และ สถาบัน จะอยู่อย่างไร",
+      },
+      {
+        id: 2,
+        timestamp: "2022/09/04",
+        userName: "B",
+        message: "เปิดเมนท์อื่นด้วยเจียม",
+      },
+      {
+        id: 3,
+        timestamp: "2022/09/04",
+        userName: "C",
+        message:
+          "บังคับให้คนรัก ทั้งๆที่ตัวมันและครอบครัว กอบโกยทุกอย่าง ไม่เคยเปนแบบอย่างที่ดีให้กับแผ่นดิน ไม่นานพวกมันก็ล่มสลายไปเอง",
+      },
+      {
+        id: 4,
+        timestamp: "2022/09/04",
+        userName: "D",
+        message: "จารเปิดคอมเม้นแล้วหรอ",
+      },
+      {
+        id: 5,
+        timestamp: "2022/09/04",
+        userName: "E",
+        message: "เสียของที่ไม่เคยมีได้ด้วยเหรอ",
+      },
+      {
+        id: 6,
+        timestamp: "2022/09/04",
+        userName: "F",
+        message: "หายใจบางแล้วเหรอเจียม",
+      },
+      {
+        id: 7,
+        timestamp: "2022/09/04",
+        userName: "G",
+        message: "คงอัดอั้นนะ ที่ สศจ ไม่เปิดเม้นให้ด่า",
+      },
+      {
+        id: 8,
+        timestamp: "2022/09/04",
+        userName: "H",
+        message:
+          "คนที่จ้องจะโจมตีเจียมจริงๆแบบหวังผลอ่ะ คือ io เพื่อไทย จะบอกให้",
+      },
+    ],
+  },
+  {
+    name: "เพื่อไทย",
     value: 12,
     commentType: "positive",
     Comment: [
-      { id: 1, timestamp: "2022/09/04", userName: "A", message: "hello world" },
-      { id: 2, timestamp: "2022/09/04", userName: "B", message: "hello world" },
-      { id: 3, timestamp: "2022/09/04", userName: "C", message: "hello world" },
+      {
+        id: 1,
+        timestamp: "2022/09/04",
+        userName: "A",
+        message: "ศาล และ สถาบัน จะอยู่อย่างไร",
+      },
+      {
+        id: 2,
+        timestamp: "2022/09/04",
+        userName: "B",
+        message: "เปิดเมนท์อื่นด้วยเจียม",
+      },
+      {
+        id: 3,
+        timestamp: "2022/09/04",
+        userName: "C",
+        message:
+          "บังคับให้คนรัก ทั้งๆที่ตัวมันและครอบครัว กอบโกยทุกอย่าง ไม่เคยเปนแบบอย่างที่ดีให้กับแผ่นดิน ไม่นานพวกมันก็ล่มสลายไปเอง",
+      },
+      {
+        id: 4,
+        timestamp: "2022/09/04",
+        userName: "D",
+        message: "จารเปิดคอมเม้นแล้วหรอ",
+      },
+      {
+        id: 5,
+        timestamp: "2022/09/04",
+        userName: "E",
+        message: "เสียของที่ไม่เคยมีได้ด้วยเหรอ",
+      },
+      {
+        id: 6,
+        timestamp: "2022/09/04",
+        userName: "F",
+        message: "หายใจบางแล้วเหรอเจียม",
+      },
+      {
+        id: 7,
+        timestamp: "2022/09/04",
+        userName: "G",
+        message: "คงอัดอั้นนะ ที่ สศจ ไม่เปิดเม้นให้ด่า",
+      },
+      {
+        id: 8,
+        timestamp: "2022/09/04",
+        userName: "H",
+        message:
+          "คนที่จ้องจะโจมตีเจียมจริงๆแบบหวังผลอ่ะ คือ io เพื่อไทย จะบอกให้",
+      },
     ],
   },
   {
-    name: "Home",
-    value: 44,
+    name: "คนรัก",
+    value: 8,
     commentType: "positive",
     Comment: [
-      { id: 1, timestamp: "2022/09/04", userName: "A", message: "hello world" },
-      { id: 2, timestamp: "2022/09/04", userName: "B", message: "hello world" },
-      { id: 3, timestamp: "2022/09/04", userName: "C", message: "hello world" },
-    ],
-  },
-  {
-    name: "Music",
-    value: 39,
-    commentType: "positive",
-    Comment: [
-      { id: 1, timestamp: "2022/09/04", userName: "A", message: "hello world" },
-      { id: 2, timestamp: "2022/09/04", userName: "B", message: "hello world" },
-      { id: 3, timestamp: "2022/09/04", userName: "C", message: "hello world" },
-    ],
-  },
-  {
-    name: "Beauty",
-    value: 29,
-    commentType: "positive",
-    Comment: [
-      { id: 1, timestamp: "2022/09/04", userName: "A", message: "hello world" },
-      { id: 2, timestamp: "2022/09/04", userName: "B", message: "hello world" },
-      { id: 3, timestamp: "2022/09/04", userName: "C", message: "hello world" },
-    ],
-  },
-  {
-    name: "Sports",
-    value: 43,
-    commentType: "positive",
-    Comment: [
-      { id: 1, timestamp: "2022/09/04", userName: "A", message: "hello world" },
-      { id: 2, timestamp: "2022/09/04", userName: "B", message: "hello world" },
-      { id: 3, timestamp: "2022/09/04", userName: "C", message: "hello world" },
-    ],
-  },
-  {
-    name: "Outdoors",
-    value: 42,
-    commentType: "positive",
-    Comment: [
-      { id: 1, timestamp: "2022/09/04", userName: "A", message: "hello world" },
-      { id: 2, timestamp: "2022/09/04", userName: "B", message: "hello world" },
-      { id: 3, timestamp: "2022/09/04", userName: "C", message: "hello world" },
-    ],
-  },
-  {
-    name: "Baby",
-    value: 39,
-    commentType: "positive",
-    Comment: [
-      { id: 1, timestamp: "2022/09/04", userName: "A", message: "hello world" },
-      { id: 2, timestamp: "2022/09/04", userName: "B", message: "hello world" },
-      { id: 3, timestamp: "2022/09/04", userName: "C", message: "hello world" },
-    ],
-  },
-  {
-    name: "Tools",
-    value: 26,
-    commentType: "positive",
-    Comment: [
-      { id: 1, timestamp: "2022/09/04", userName: "A", message: "hello world" },
-      { id: 2, timestamp: "2022/09/04", userName: "B", message: "hello world" },
-      { id: 3, timestamp: "2022/09/04", userName: "C", message: "hello world" },
-    ],
-  },
-  {
-    name: "Toys",
-    value: 49,
-    commentType: "positive",
-    Comment: [
-      { id: 1, timestamp: "2022/09/04", userName: "A", message: "hello world" },
-      { id: 2, timestamp: "2022/09/04", userName: "B", message: "hello world" },
-      { id: 3, timestamp: "2022/09/04", userName: "C", message: "hello world" },
-    ],
-  },
-  {
-    name: "Clothing",
-    value: 28,
-    commentType: "positive",
-    Comment: [
-      { id: 1, timestamp: "2022/09/04", userName: "A", message: "hello world" },
-      { id: 2, timestamp: "2022/09/04", userName: "B", message: "hello world" },
-      { id: 3, timestamp: "2022/09/04", userName: "C", message: "hello world" },
+      {
+        id: 1,
+        timestamp: "2022/09/04",
+        userName: "A",
+        message: "ศาล และ สถาบัน จะอยู่อย่างไร",
+      },
+      {
+        id: 2,
+        timestamp: "2022/09/04",
+        userName: "B",
+        message: "เปิดเมนท์อื่นด้วยเจียม",
+      },
+      {
+        id: 3,
+        timestamp: "2022/09/04",
+        userName: "C",
+        message:
+          "บังคับให้คนรัก ทั้งๆที่ตัวมันและครอบครัว กอบโกยทุกอย่าง ไม่เคยเปนแบบอย่างที่ดีให้กับแผ่นดิน ไม่นานพวกมันก็ล่มสลายไปเอง",
+      },
+      {
+        id: 4,
+        timestamp: "2022/09/04",
+        userName: "D",
+        message: "จารเปิดคอมเม้นแล้วหรอ",
+      },
+      {
+        id: 5,
+        timestamp: "2022/09/04",
+        userName: "E",
+        message: "เสียของที่ไม่เคยมีได้ด้วยเหรอ",
+      },
+      {
+        id: 6,
+        timestamp: "2022/09/04",
+        userName: "F",
+        message: "หายใจบางแล้วเหรอเจียม",
+      },
+      {
+        id: 7,
+        timestamp: "2022/09/04",
+        userName: "G",
+        message: "คงอัดอั้นนะ ที่ สศจ ไม่เปิดเม้นให้ด่า",
+      },
+      {
+        id: 8,
+        timestamp: "2022/09/04",
+        userName: "H",
+        message:
+          "คนที่จ้องจะโจมตีเจียมจริงๆแบบหวังผลอ่ะ คือ io เพื่อไทย จะบอกให้",
+      },
     ],
   },
 ];
