70b3b726ab0efa585be68854cf194a7981def4b1
ภูริพัฒน์ กมลเนตร์ <phooriphat.k@rtarf.mi.th>
2024-03-12 16:48:34 +0700

    add more accManageAPI

--- 3DJsExp/src/components/AccountManage/AccountManage.jsx
+++ 3DJsExp/src/components/AccountManage/AccountManage.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
-import { Select } from "antd";
-import { DataTable } from "../../utilities";
+
+import { Select, Modal, Button, Input } from "antd";
+import { DataTable, Loading } from "../../utilities";
 import classNames from "classnames";
 import { useResponsive } from "../../hooks";
 import Image from "../../assets/PostImage";
 import AccountManageAPI from "../../service/AccountManageAPI";
 import { useSelector } from "react-redux";
 import { getLogin } from "../../libs/loginSlice";
+import TextArea from "antd/es/input/TextArea";
+import { EditOutlined } from "@ant-design/icons";
 
 const columns = [
   {
@@ -24,7 +27,7 @@ const columns = [
     key: "name",
     align: "center",
     width: 150,
-    className: "tw-text-lime-600",
+    // className: "tw-text-lime-600",
     render: (text, record) => <p>{record.title}</p>,
   },
   {
@@ -42,25 +45,40 @@ const columns = [
     align: "center",
     width: 200,
     className: "tw-text-violet-600",
-    render: (text, record) =>
-      record.information.profile_tabs.map((val, index) => (
-        <p key={index}>{val} </p>
-      )),
+    // render: (text, record) =>
+    //   record.information.profile_tabs.map((val, index) => (
+    //     <p key={index}>{val} </p>
+    //   )),
   },
   {
     title: "Link",
     dataIndex: "link",
     key: "link",
     align: "center",
-    width: 150,
+    width: 50,
     className: "tw-text-amber-600",
     render: (text, record) => (
       <div className="tw-flex tw-justify-center ">
         <a href={record.url} target="blank">
-          <div className="hover:tw-bg-blue-100 tw-rounded-full tw-p-1">
+          <div className="hover:tw-bg-blue-200 tw-rounded-full tw-p-1">
             <img className="tw-w-7 tw-h-7 " src={Image.link} />
           </div>
         </a>
+      </div>
+    ),
+  },
+  {
+    title: "",
+    dataIndex: "",
+    key: "",
+    align: "center",
+    width: 50,
+    className: "tw-text-blue-600",
+    render: (text, record) => (
+      <div className="tw-flex tw-justify-center ">
+        <div className="hover:tw-bg-blue-200 tw-rounded-full tw-p-1 tw-flex tw-justify-center">
+          <EditOutlined className="tw-w-7 tw-h-7 tw-text-2xl" />
+        </div>
       </div>
     ),
   },
@@ -68,6 +86,10 @@ const columns = [
 
 const AccountManage = () => {
   const [accInfo, setAccInfo] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editName, setEditName] = useState("")
+  const [editDetail, setEditDetail] = useState("")
+
   const { isTabletOrMobile, isMobile, isPortrait, isLandscape } =
     useResponsive();
   console.log(accInfo);
@@ -79,6 +101,24 @@ const AccountManage = () => {
   const onSearch = (value) => {
     console.log("search:", value);
   };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const handleNameInput = (event) => {
+    const inputValue = event.target.value;
+    setEditName(inputValue)
+  };
+  const handleDetailInput = (event) => {
+    const inputValue = event.target.value;
+    setEditDetail(inputValue)
+  }
 
   useEffect(() => {
     const getData = async () => {
@@ -94,6 +134,25 @@ const AccountManage = () => {
   return (
     <div className="tw-w-full tw-h-full tw-flex tw-flex-col tw-items-center ">
       <div className=" tw-w-[80%] tw-max-h-full tw-flex-row tw-flex tw-gap-4 tw-justify-center">
+        <Select
+          className="tw-w-[30%] tw-outline-blue-500 tw-outline tw-rounded-md "
+          showSearch
+          placeholder="ประเภท"
+          optionFilterProp="children"
+          onChange={onChange}
+          onSearch={onSearch}
+          filterOption={filterOption}
+          options={[
+            {
+              value: "profile",
+              label: "profile",
+            },
+            {
+              value: "group",
+              label: "group",
+            },
+          ]}
+        />
         <Select
           className="tw-w-full tw-outline-blue-500 tw-outline tw-rounded-md "
           showSearch
@@ -140,16 +199,97 @@ const AccountManage = () => {
             },
           ]}
         />
-        <button className="tw-w-[50%] tw-h-8 tw-bg-green-500 tw-rounded-lg tw-items-center tw-text-white">
+        <button
+          onClick={showModal}
+          className="tw-w-[50%] tw-h-8 tw-bg-green-500 tw-rounded-lg tw-items-center tw-text-white"
+        >
           เพิ่มเป้าหมายใหม่
         </button>
       </div>
       <div
-        className={classNames("tw-border-2 tw-rounded-md tw-mt-4", {
-          "tw-overflow-auto tw-min-h-fit": isTabletOrMobile && isPortrait,
-        })}
+        className={classNames(
+          "tw-border-2 tw-rounded-md tw-mt-4 tw-overflow-x-auto tw-min-w-fit",
+          {
+            "tw-overflow-auto tw-min-h-fit": isTabletOrMobile && isPortrait,
+          }
+        )}
       >
         <DataTable columns={columns} data={accInfo} />
+        <Modal
+          title="เพิ่มเป้าหมายใหม่"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          centered={true}
+          footer={[
+            <Button
+              key="submit"
+              className="tw-bg-blue-500 tw-text-white tw-border-white hover:tw-bg-white hover:tw-text-blue-500 hover:tw-border-blue-500"
+              // onClick={() => handleSave()}
+            >
+              บันทึก
+            </Button>,
+          ]}
+        >
+          <p>ประเภท:</p>
+          <Select
+            className="tw-w-full  tw-rounded-md "
+            showSearch
+            placeholder="ประเภท"
+            optionFilterProp="children"
+            onChange={onChange}
+            onSearch={onSearch}
+            filterOption={filterOption}
+            options={[
+              {
+                value: "profile",
+                label: "profile",
+              },
+              {
+                value: "group",
+                label: "group",
+              },
+              {
+                value: "page",
+                label: "page",
+              },
+            ]}
+          />
+          <p>ชื่อ:</p>
+          <Input
+            className="tw-w-full  tw-rounded-md "
+            placeholder="กรอกชื่อ"
+            onChange={handleNameInput}
+          />
+          <p>กลุ่ม:</p>
+          <Select
+            className="tw-w-full   tw-rounded-md "
+            showSearch
+            placeholder="กลุ่ม"
+            optionFilterProp="children"
+            onChange={onChange}
+            onSearch={onSearch}
+            filterOption={filterOption}
+            options={[
+              {
+                value: "A",
+                label: "A",
+              },
+              {
+                value: "B",
+                label: "B",
+              },
+              {
+                value: "C",
+                label: "C",
+              },
+            ]}
+          />
+          <p>รายละเอียด:</p>
+          <TextArea rows={4} onChange={handleDetailInput} />
+          <p>Link:</p>
+          <Input className="tw-w-full tw-rounded-md " placeholder="กรอก link" />
+        </Modal>
       </div>
     </div>
   );
--- 3DJsExp/src/service/AccountManageAPI.js
+++ 3DJsExp/src/service/AccountManageAPI.js
@@ -23,6 +23,72 @@ const AccountManageAPI = {
       throw error;
     }
   },
+  getGroupProfile: async (token) => {
+    try {
+      const response = await axios.get(
+        `http://192.168.10.111:8000/facebook/get_group_profile/`,
+        {
+          headers: {
+            Authorization: "Bearer " + token.token,
+          },
+        }
+      );
+
+      return response.data; // Return the response data
+    } catch (error) {
+      // console.log(error);
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      }
+      throw error;
+    }
+  },
+  upDateLabelProfile: async (url,token) => {
+    try {
+      const response = await axios.post(
+        `http://192.168.10.111:8000/facebook/update_label_profile/?url=${url}`,
+        {
+          headers: {
+            Authorization: "Bearer " + token.token,
+          },
+        }
+      );
+
+      return response.data; // Return the response data
+    } catch (error) {
+      // console.log(error);
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      }
+      throw error;
+    }
+  },
+  upDateLabelGroupProfile: async (url,token) => {
+    try {
+      const response = await axios.post(
+        `http://192.168.10.111:8000/facebook/update_label_group_profile/?url=${url}`,
+        {
+          headers: {
+            Authorization: "Bearer " + token.token,
+          },
+        }
+      );
+
+      return response.data; // Return the response data
+    } catch (error) {
+      // console.log(error);
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      }
+      throw error;
+    }
+  },
 };
 
 export default AccountManageAPI;
